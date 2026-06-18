3a68ee75d63d4ef005892eb33b9ab6c3e514907b
Adrian <adrianwelter1@gmail.com>
2026-06-18 21:52:09 +0200

    Refined training + eval structure: model specific train + eval dirs under outputs/; models/ for pretrained weights; visualize random samples correction to show bbxs+labels

--- src/model.py
+++ src/model.py
@@ -2,14 +2,19 @@
 Init YOLO model, call train & eval
 """
 
+from pathlib import Path
+
 from ultralytics import YOLO
+from ultralytics.utils.metrics import DetMetrics
 
 
-def load_model(weights: str) -> YOLO:
+def load_model(
+        weights: str,
+        weights_dir: str) -> YOLO:
     """
     Load YOLO model with pretrained weights.
     """
-    return YOLO(weights)
+    return YOLO(weights_dir + "/" + weights)
 
 def train(
         model: YOLO,
@@ -18,24 +23,57 @@ def train(
     """
     Call preimplemented YOLO train function.
     """
-    model.train(
-        data=config["data"]["yolo_yaml_path"],
-        epochs=config["train"]["epochs"],
-        batch=config["train"]["batch_size"],
-        imgsz=config["model"]["imgsz"],
-        seed=config["seed"])
+
+    model_out_dir = Path(config["model"]["out_dir"]) / config["model"]["model_name"] / config["model"]["train_subdir"]
+            
+    model_last_cp_path = model_out_dir / "weights" / "last.pt"
+    
+    if model_last_cp_path.exists():
+        resume_train(
+            weights=str(model_last_cp_path.name),
+            weights_dir=str(model_last_cp_path.parent)
+            )
+    else:
+        model_out_dir.mkdir(parents=True, exist_ok=True)
+        model.train(
+            data=config["data"]["yolo_yaml_path"],
+            epochs=config["train"]["epochs"],
+            batch=config["train"]["batch_size"],
+            imgsz=config["model"]["imgsz"],
+            seed=config["seed"],
+            save_dir=model_out_dir
+            ) 
     
 
 def eval(
         model: YOLO,
         config: dict
-        ) -> None:
+        ) -> DetMetrics:
     """
     Call preimplemented YOLO val function.
     """
+    model_out_dir = Path(config["model"]["out_dir"]) / config["model"]["model_name"] / config["model"]["eval_subdir"]
+
     metrics = model.val(
         data=config["data"]["yolo_yaml_path"],
-        imgsz=config["train"]["imgsz"]
+        save_dir=model_out_dir
     )
 
-    print(metrics)
+    return metrics
+
+def resume_train(
+        weights: str,
+        weights_dir: str) -> None:
+    """
+    Ask user to confirm resume training with exit and train option.
+    """
+    user_out = input(f"Found a model under '{weights_dir}'.\n \
+                        Continue training (y) or exit (n)?")
+    while user_out not in ["y", "n"]:
+        print("Choose a valid option ('y' or 'n').")
+    if user_out == "n":
+        return
+    else:
+        model = load_model(weights=weights,
+                           weights_dir=weights_dir)
+        model.train(resume=True)
--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,13 @@ Utility + helper functions
 """
 
 import yaml
+import random
 from pathlib import Path
+from PIL import Image
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 def load_config(
         path: Path | str = "configs/config.yaml"
@@ -52,3 +58,80 @@ def write_yolo_yaml(
         yaml.dump(content, f, sort_keys=False)
 
     print(f"[INFO] YOLO data config written to '{yolo_yaml_path}'")
+
+
+def get_lbl_arrays(lbl_path):
+    """ 
+    Get class (int) + bbox (float) arrays from txt-label file
+    """
+    lbl_data = Path(lbl_path).read_text().split()
+    
+    if len(lbl_data) % 5 != 0:
+        raise ValueError(f"Inconsistent label format for '{lbl_path}'")
+    
+    lbl_arr = np.array(lbl_data, dtype=np.float64).reshape(-1, 5)
+    classes = lbl_arr[:, :1].astype(int).flatten()
+    bbxs = lbl_arr[:, 1:]
+    
+    return classes, bbxs
+
+
+def visualize_sample(img_path, lbl_path, idx_to_class, ax=None):
+    """
+    Visualize a sample and its labeled bbxs.
+    """
+
+    if ax is None:
+        _, ax = plt.subplots(1,1,figsize=(10,10))
+    
+    colors = {0: "green", 1: "red"}
+    counts = {0: 0, 1: 0}
+    
+    img = Image.open(img_path)
+    W,H = img.size
+
+    classes, bbxs = get_lbl_arrays(lbl_path)
+
+    for c,(x_c, y_c, w, h) in zip(classes, bbxs):
+
+        w, h = w * W, h * H
+        x_c, y_c = x_c * W, y_c * H
+        x_min, y_min = x_c - w/2 , y_c - h/2
+
+        counts[c] += 1
+
+        ax.imshow(img)
+        ax.add_patch(Rectangle((x_min,y_min), w, h, fc ='none', ec=colors[c], lw=2))
+
+        ax.text(
+                x_min, y_min, idx_to_class[c],
+                color="white", fontsize=9, va="bottom", ha="left",
+                bbox=dict(facecolor=colors[c], edgecolor="none", pad=1.5),
+            )
+
+    ax.axis("off")
+
+
+def inspect_rnd_samples(k=4):
+    """
+    Visualize k random data-label pairs
+    """
+    
+    # Get random image path
+    data_dir = Path("data/raw/agri_data/data")
+    img_paths = list(data_dir.glob("*.jpeg"))
+    rnd_img_paths = random.sample(img_paths, k)
+
+    # Get class mapping
+    classes_path = Path("data/raw/classes.txt")
+    classes = open(classes_path, "r").read().split()
+    idx_to_class = {i: c for i,c in enumerate(classes)}
+ 
+    _, axs = plt.subplots(1,k,figsize=(5*k,5))
+
+    for img_path, ax in zip(rnd_img_paths, axs):
+        # Get label path
+        img_id = img_path.stem
+        lbl_path = data_dir / (img_id +".txt")
+
+        visualize_sample(img_path, lbl_path, idx_to_class, ax)
